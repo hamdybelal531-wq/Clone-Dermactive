00e3a72c1f7974899a4810ce1d2f18438e163ff9
hamdybelal531-wq <hamdybelal531@gmail.com>
2025-11-19 21:43:34 +0200

    Update ourRangeH.js

--- src/app/componts/navebarhover/ourRangeH.js
+++ src/app/componts/navebarhover/ourRangeH.js
@@ -119,7 +119,7 @@ export default function HoverOurRange({ Show, SetShow }) {
       {Show.ShowOurRange && (
         <div
           onMouseLeave={() => SetShow({ ...Show, ShowOurRange: false })}
-          className="absolute top-18 left-0 w-full h-120 bg-white pb-10 flex z-[1000]"
+          className="absolute top-18 left-0 w-full h-120 bg-white pb-10 flex z-[9999]"
         >
           <div className="pb-10">
             <ul className="p-10 flex flex-col gap-5">
