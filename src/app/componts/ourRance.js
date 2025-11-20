d0d3b56edf5ffe664b5e3927ce0a77a3a6336b78
Belal Hamdy <hamdybelal513@yahoo.com>
2025-11-20 19:31:10 +0200

    DDDA

--- src/app/componts/ourRance.js
+++ src/app/componts/ourRance.js
@@ -76,8 +76,6 @@ export default function OurRance() {
         minimumTouchDrag={5}
         pauseOnHover={true}
         containerClass="carousel-container"
-        // customTransition={`transform 0.15s ${SMOOTH_EASING}`}
-        // transitionDuration={150}
         responsive={responsive}
         className=" pb-16 text-center "
         itemClass=""
