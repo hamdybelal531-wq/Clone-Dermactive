80b64fdec9bd0210a2371269dad6fda55ae11f3e
Belal Hamdy <hamdybelal513@yahoo.com>
2025-11-25 02:00:12 +0200

    add

--- src/app/componts/navebarhover/sidbarmenu.js
+++ src/app/componts/navebarhover/sidbarmenu.js
@@ -449,7 +449,7 @@ export default function MobileMenu({ ShowMobileMenu, SetShowMobileMenu }) {
           animate={{ y: 0 }}
           exit={{ y: "-100%" }}
           transition={{ duration: 0.3 }}
-          className="fixed top-0 left-2 w-full max-w-xs md:max-w-lg bg-white shadow-xl z-[999999999] lg:hidden"
+          className="fixed top-0 left-2 w-full max-w-xs md:max-w-lg bg-white shadow-xl z-[999999999] lg:hidden overflow-y-auto h-screen"
         >
           {/* Header & Close Button */}
           <div className="p-4 flex justify-between items-center border-b border-gray-200 sticky top-0 bg-white">
